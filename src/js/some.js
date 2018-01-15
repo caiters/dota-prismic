ee8b5815d8daedd4cab61754267f2af124c72210
Caitlin <ayashi@valefor.com>
2018-01-14 23:26:07 -0800

    starting styling mobile nav, adding js for menu btn

--- src/js/some.js
+++ src/js/some.js
@@ -1,3 +1,6 @@
 (() => {
-  console.log('helloooo');
+  const navToggle = document.querySelector('.js--nav-toggle');
+  navToggle.addEventListener('click', (e) => {
+    navToggle.classList.toggle('nav-toggle--open');
+  });
 })();
