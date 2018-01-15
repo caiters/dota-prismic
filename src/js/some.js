72aa8987c5c6a4ba4f717314fe01517dae6d6f01
Caitlin <ayashi@valefor.com>
2018-01-14 23:40:37 -0800

    more mobile nav stuff, probably need to rework it some

--- src/js/some.js
+++ src/js/some.js
@@ -1,6 +1,17 @@
 (() => {
   const navToggle = document.querySelector('.js--nav-toggle');
+  const mainNav = document.querySelector('.main-nav');
+  const siteBody = document.querySelector('.site-wrapper');
+
+  // mobile nav
+  function toggleMainNav() {
+    mainNav.classList.toggle('main-nav--open');
+  }
+
+  // mobile nav
   navToggle.addEventListener('click', (e) => {
     navToggle.classList.toggle('nav-toggle--open');
+    siteBody.classList.toggle('site-wrapper--nav-open');
+    toggleMainNav();
   });
 })();
