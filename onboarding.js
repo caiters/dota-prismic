d4e9654795dd4531e3348512061fc6aee98621ef
Caitlin <ayashi@valefor.com>
2018-03-10 13:20:56 -0800

    nevermind

--- onboarding.js
+++ onboarding.js
@@ -7,7 +7,7 @@ const request = require('request');
 function trigger() {
   const repoRegexp = /^(https?:\/\/([-\w]+)\.[a-z]+\.(io|dev))\/api(\/v2)?$/;
   const [_, endpoint] = PrismicConfig.apiEndpoint.match(repoRegexp);
-  request.post(`${endpoint}/app/settings/onboarding/run`, { form: { language: 'node', framework: 'express', 'mode': 'no-cors' } });
+  request.post(`${endpoint}/app/settings/onboarding/run`, { form: { language: 'node', framework: 'express' } });
 }
 
 module.exports = {
